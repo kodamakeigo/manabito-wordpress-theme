b38acac659db246adea242bf75404c2dead954f7
児玉恵吾 <k.kei5.0205@gmail.com>
2021-02-23 21:06:26 +0900

    お問合せ・固定ページ設定

--- page.php
+++ page.php
@@ -1,7 +1,24 @@
 <?php get_header(); ?>
-	<div class="l-contents-wrapper">
-		<main></main>
-		<?php get_sidebar(); ?>
-		<nav id="page_top" class="page-top">ページの先頭へ戻る</nav>
-	</div>
+<?php
+if ( shortcode_exists( 'wp-structuring-markup-breadcrumb' ) ) {
+	echo'<nav class="breadcrumb">';
+	echo do_shortcode( '[wp-structuring-markup-breadcrumb]' );
+	echo'</nav>';
+}
+?>
+<div class="l-contents-wrapper">
+	<main>
+		<?php
+		if ( have_posts() ) :
+			while ( have_posts() ):
+				the_post();
+				the_title( '<h1>', '</h1>' );
+				the_content();
+			endwhile;
+		endif;
+		?>
+	</main>
+	<?php get_sidebar(); ?>
+	<nav id="page_top" class="page-top">ページの先頭へ戻る</nav>
+</div>
 <?php get_footer();
